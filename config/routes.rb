70793b0265ea7aa5320320d56407a14ca0f11eb2
Cora Quezada <corafenice1013@gmail.com>
2018-04-30 14:26:59 -0400

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'urls#index'
+  get '/urls', to: 'urls#index'
+  get '/:id', to: 'urls#show'
   resources :urls
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details oon the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
