74b473724ccbe4fd84be30505a98d527df75c64f
Cora Quezada <corafenice1013@gmail.com>
2018-04-30 14:27:34 -0400

    created view , almost done

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,8 @@
 class Url < ApplicationRecord
+validates :long, :short, uniqueness: true
+before_validation :get_short, on: :create
+
+def get_short
+self.short = ('a'..'z').to_a.shuffle[0,2].join << SecureRandom.hex(1)
+end
 end
--- app/views/urls/_url.json.jbuilder
+++ app/views/urls/_url.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! url, :id, :created_at, :updated_at
+json.extract! url, :id, :long, :short, :created_at, :updated_at
 json.url url_url(url, format: :json)
